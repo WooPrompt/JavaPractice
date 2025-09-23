e94fd43092ec03355d50dd8d5a042d595332c9bb
WooPrompt <woo20250709@gmail.com>
2025-09-23 12:48:40 +0900

    Practice - ArrayList (Extending a class ) 1. Overriding 2. instance type check 3. Type Casting.

--- src/day11/ShapeTest.java
+++ src/day11/ShapeTest.java
@@ -1,5 +1,7 @@
 package day11;
 
+import java.util.ArrayList;
+
 class Shape {
     void draw() {
         System.out.println("drawing..");
@@ -11,6 +13,9 @@ class Circle extends Shape {
     @Override
     void draw() {
         System.out.println("drawing circle");
+    }
+    void print(){
+        System.out.println("print");
     }
 }
 
@@ -30,13 +35,37 @@ class Rectangular extends Shape {
 
 public class ShapeTest {
     public static void main(String[] args) {
+        ArrayList<Shape> shapeList = new ArrayList<>();
         Shape shape = new Shape();
         shape.draw();
 
         Circle circle = new Circle();
         circle.draw();
+
+        Triangle triangle = new Triangle();
+        Rectangular rect = new Rectangular();
         System.out.println(circle instanceof Circle);
         System.out.println(circle instanceof Shape);
 //        System.out.println(circle instanceof Triangle);
+
+        shapeList.add(shape);
+        shapeList.add(circle);
+        shapeList.add(triangle);
+        shapeList.add(rect);
+
+        for(Shape s : shapeList){
+            s.draw();
+        } for(Shape s : shapeList){
+
+            //check the instance type
+            if (s instanceof Circle){
+                //type conversion
+                Circle circleType = (Circle)s;
+                circleType.print();
+            }else {
+                System.out.println("Not Supported action");
+            }
+        }
+
     }
 }
