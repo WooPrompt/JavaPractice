cfb491226e488ad886a940ce042bdba6f3b07ba6
WooPrompt <woo20250709@gmail.com>
2025-09-24 12:20:37 +0900

    Practice - Object equals overriding

--- src/day12/school/Person.java
+++ src/day12/school/Person.java
@@ -1,14 +1,28 @@
 package day12.school;
 
+import java.util.Objects;
+
 public class Person {
     String name;
     int age;
 
-    public Person (String name, int age){
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
-    public void printProfile(){
-        System.out.printf("나이 : %d , 이름 : %s",age,name);
+
+    public void printProfile() {
+        System.out.printf("나이 : %d , 이름 : %s", age, name);
     }
+
+    @Override
+    public boolean equals(Object object) {
+        if ((object instanceof Person)) {
+            Person person = (Person) object;
+            return name.equals(person.name);
+        } else {
+            return false;
+        }
+    }
+
 }
--- src/day12/school/SchoolTest.java
+++ src/day12/school/SchoolTest.java
@@ -3,12 +3,14 @@ package day12.school;
 public class SchoolTest {
     public static void main(String[] args) {
         Manager manager = new Manager(10);
-        Teacher teacher = manager.makeTeacherInstance("kim",30,"English");
+        Teacher teacher = manager.makeTeacherInstance("yu",30,"English");
         Student student1 = manager.makeStudentInstance("yu",14);
-        Student student2 = manager.makeStudentInstance();
-        Teacher teacher2 = manager.makeTeacherInstance();
+//        Student student2 = manager.makeStudentInstance();
+//        Teacher teacher2 = manager.makeTeacherInstance();
 
-        manager.printWholeProfile();
+//        manager.printWholeProfile();
+        boolean result = student1.equals(teacher);
+        System.out.println(result);
 
     }
 }
