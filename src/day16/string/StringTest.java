1cef86b5a4880fe4d603321198eb77f147df4bd8
WooPrompt <woo20250709@gmail.com>
2025-09-30 11:15:59 +0900

    Practice - String pool,String fomatting , text block , concat , string equals

---
+++ src/day16/string/StringTest.java
@@ -0,0 +1,50 @@
+package day16.string;
+
+public class StringTest {
+
+    public static void main(String[] args) {
+        String str1 = new String("abc");
+        String str2 = new String("abc");
+        System.out.println(str2 == str1);//false
+        System.out.println(str1.equals(str2));//true
+
+// equals() method in String:
+// public boolean equals(Object anObject) {
+//     if (this == anObject) { // checks if references are the same
+//         return true;
+//     }
+//     return (anObject instanceof String aString) // if the object is a String
+//             && (!COMPACT_STRINGS || this.coder == aString.coder)
+//             && StringLatin1.equals(value, aString.value); // compares the contents
+// }
+
+        System.out.println(str2 == str1); // false: compares memory addresses (two different objects)
+        System.out.println(str1.equals(str2)); // true: compares values, both are "abc"
+
+
+// String pool
+        String str3 = "abc"; // Java checks if "abc" already exists in the string pool.
+        String str4 = "abc";
+
+        System.out.println(str3 == str4); // true: both point to the same pooled string
+        System.out.println(str3.equals(str4)); // true: contents are the same
+
+// String formatting with formatted()
+        String studentInfo = "%s is \n%d grade \nscore is %.1f.".formatted("Alex", 2, 3.5);
+        System.out.println(studentInfo);
+
+// Text Block (Java 15+)
+        String studentInfo2 = """
+                %s is 
+                %d grade 
+                score is %.1f.""".formatted("Alex", 2, 3.5);
+        System.out.println(studentInfo2);
+
+//concat method
+        String hello = "hello";
+        String world = "world";
+        String message = hello.concat(world);
+        System.out.println(message);
+
+    }
+}
